bae9f82238e15422108d609bf505133281814d8b
Stewart Park <stewartpark92@gmail.com>
2015-05-01 02:14:24 -0700

    Added `Flask-Script`.

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,3 @@ app.secret_key = '/xcbq^/x0c$/x9f/xd9/xe72/xb33uX/xfb/xd3x/x0f/xf3aH/xf9e/x06e'
 
 from app import views
 from app import models
-
-if len(models.User.query.all()) == 0:
-    u = models.User()
-    u.username = 'test'
-    u.email = 'test@email.com'
-    u.password = 'pass'
-    db.session.add(u)
-    db.session.commit()
--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 #!flask/bin/python
-from app import app
-app.run(debug=True)
+from flask.ext.script import Manager
+from app import app, db, models
+
+manager = Manager(app)
+
+@manager.command
+def create_all():
+    db.create_all()
+
+@manager.command
+def create_user(nickname, email, password):
+    with app.app_context():
+        u = models.User()
+        u.nickname = nickname
+        u.password = password
+        u.email = email
+        db.session.add(u)
+        db.session.commit()
+
+if __name__ == '__main__':
+    manager.run()
